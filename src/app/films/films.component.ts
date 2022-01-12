9321e9dcaab49cb5e88f957fcffc260e48eff0d1
Pierrot <piballz@gmail.fr>
2022-01-12 09:17:48 +0100

    api connected

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsComponent implements OnInit {
 
+  title: string = 'hulk';
   filmData: any;
+
+  
 
   constructor() { }
 
@@ -19,25 +22,30 @@ export class FilmsComponent implements OnInit {
   // recuperer les donnees de l'API
   getFilmData(): void{
 
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=90eee229`)
+    fetch(`http://www.omdbapi.com/?t=${this.title}&i=tt3896198&apikey=90eee229`)
     //promesse recuperer la reponse au format Json
-      .then(response =>response.json())
+      .then(response => response.json())
+      // .then(function unNom(response){return response.json})
 
-      .then(data=>this.setFilmData(data));
+      .then(data => this.setFilmData(data));
   }
 
-//method on envoie les data
-setFilmData(data:any):void{
-  this.filmData = data;
+  //method on envoie les data
+  setFilmData(data:any):void{
+    this.filmData = data;
+   //
 
-  //
- console.log(this.filmData.Title);
- console.log(this.filmData.Year);
+  console.log(this.filmData.title);
+  // console.log(this.filmData.Year);
+  console.log(this.filmData);
+  // console.log(this.filmData.Search[1].Title);
+   
+  }
 
-
-}
-
-
+  // chercher le film
+  searchFilm(): void {
+    this.getFilmData();
+  }
 
 
 }
